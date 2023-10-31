af95c6f57240f46e9aab50ce5a4c93a77b086fdb
DionChen <s8800242002@gmail.com>
2023-10-31 18:30:21 +0800

    first commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+create table Currency (
+                      id INT not null AUTO_INCREMENT,
+                      code varchar(100) not null UNIQUE,
+                      ch_name varchar(100),
+                      rate decimal(10, 4),
+                      description varchar(100),
+                      active boolean,
+                      created_by varchar(100),
+                      created_date date,
+                      lastModified_by varchar(100),
+                      lastModified_date date,
+                      PRIMARY KEY (id)
+);
